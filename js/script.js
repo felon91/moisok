f0fed42a3b84ca967dc413d544343376de7c1da6
felon91 <felon_91@mail.ru>
2019-05-17 15:37:39 +0300

    update verstka

--- js/script.js
+++ js/script.js
@@ -45,4 +45,57 @@ $(document).ready(function () {
     return false;
   });
 
+  //Modal-form
+  $('.container').on("click", '.header__button, .analytics__button', function (e) {
+    e.preventDefault();
+
+    if ($(".form-modal--form").hasClass('form-modal--active')) {
+      $(".form-modal--form").removeClass('form-modal--active');
+    } else {
+      $(".form-modal--form").addClass('form-modal--active');
+    }
+  });
+
+  $('.container').on("click", '.video__item', function (e) {
+    e.preventDefault();
+
+    let url = $(this).attr('data-url');
+
+    if ($(".form-modal--video").hasClass('form-modal--active')) {
+      $(".form-modal--video").removeClass('form-modal--active');
+      $(".form-modal__video").html('');
+    } else {
+      $(".form-modal--video").addClass('form-modal--active');
+      $(".form-modal__video").html(url);
+    }
+  });
+
+  //Modal-form close
+  $('.container').on("click", '.form-modal__close', function (e) {
+    e.preventDefault();
+    $(".form-modal").removeClass('form-modal--active');
+    $(".form-modal__video").html('');
+  });
+
+  //Modal-popup, modal-form close
+  $('.container').on("click", '.form-modal__center', function (e) {
+    if ($(e.target).hasClass('form-modal__center')) {
+      $(".form-modal").removeClass('form-modal--active');
+      $(".form-modal__video").html('');
+    }
+  });
+
+  //Modal-popup, modal-form close
+  $('.container').on("click", '.hamburger', function (e) {
+    if ($(".menu").hasClass('menu--active')) {
+      $(".menu").removeClass('menu--active');
+      $(".menu").slideUp(500);
+      $(this).removeClass('hamburger--active');
+    } else {
+      $(".menu").addClass('menu--active');
+      $(".menu").slideDown(500);
+      $(this).addClass('hamburger--active');
+    }
+  });
+
 });
